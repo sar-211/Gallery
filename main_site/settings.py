b1e3a9842bec0b241c998838cda5d3e8b8e95d16
sar-211 <sharurana103@gmail.com>
2024-04-26 13:22:05 -0400

    whitenoise added

--- main_site/settings.py
+++ main_site/settings.py
@@ -45,12 +45,15 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    #heroku
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    
 ]
 
 ROOT_URLCONF = 'main_site.urls'
